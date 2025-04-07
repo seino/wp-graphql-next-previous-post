e03ee40ae042f0d6b12f6b9ad16ae96749c7deb4
seino <akihiro.seino@gmail.com>
2025-04-07 13:03:45 +0900

    Add original repository link and document taxonomy features
    - Added link to the original repository (m-inan/wp-graphql-next-previous-post)
    - Documented features for retrieving next/previous posts in the same category
    - Added examples for using custom taxonomies
    - Updated installation instructions

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # WPGraphql Next-Previous Post
 
-When using `wp-graphql`, you can use this package to bring the next and previous articles in the post.
+This project is a fork of [m-inan/wp-graphql-next-previous-post](https://github.com/m-inan/wp-graphql-next-previous-post).
+
+When using `wp-graphql`, you can use this package to bring the next and previous articles in the post, including those in the same category or taxonomy.
 
 ### Installation
 
 ```sh
 cd wp-content/plugins
 
-git clone --branch master https://github.com/m-inan/wp-graphql-next-previous-post.git
+git clone --branch master https://github.com/seino/wp-graphql-next-previous-post.git
 ```
 
 ### Usage
+
+#### Standard next/previous posts:
 
 ```graphql
 query Post {
@@ -26,13 +30,45 @@ query Post {
 }
 ```
 
+#### Next/previous posts in the same category:
+
+```graphql
+query PostInCategory {
+    post(id: 1, idType: DATABASE_ID) {
+        title
+        nextInCategory {
+            title
+        }
+        previousInCategory {
+            title
+        }
+    }
+}
+```
+
+#### Next/previous posts in a custom taxonomy:
+
+```graphql
+query PostInTaxonomy {
+    post(id: 1, idType: DATABASE_ID) {
+        title
+        nextInCategory(taxonomy: "custom_taxonomy") {
+            title
+        }
+        previousInCategory(taxonomy: "custom_taxonomy") {
+            title
+        }
+    }
+}
+```
+
 ### Dependencies
 
 No Dependencies.
 
 ### Reporting Issues
 
-If believe you've found an issue, please [report it](https://github.com/m-inan/wp-graphql-next-previous-post/issues) along with any relevant details to reproduce it.
+If believe you've found an issue, please [report it](https://github.com/seino/wp-graphql-next-previous-post/issues) along with any relevant details to reproduce it.
 
 ### Asking for help
 
--- src/Loader.php
+++ src/Loader.php
@@ -9,6 +9,7 @@ use WPGraphQL\Model\Post;
  * Class Loader
  *
  * This class allows you to see the next and previous posts in the 'post' type.
+ * Added functionality to also get next and previous posts within the same category.
  *
  * @package WNextPreviousPost
  * @since   0.1.0
@@ -48,6 +49,7 @@ class Loader
         $typeNames[] = 'Post';
 
         foreach ($typeNames as $typeName) {
+            // Standard next post field (existing functionality)
             register_graphql_field($typeName, 'next', [
                 'type' => $typeName,
                 'description' => __('Next post'),
@@ -78,10 +80,10 @@ class Loader
                 },
             ]);
 
+            // Standard previous post field (existing functionality)
             register_graphql_field($typeName, 'previous', [
                 'type' => $typeName,
                 'description' => __('Previous post'),
-
                 'resolve' => function (
                     Post $post,
                     array $args,
@@ -96,6 +98,90 @@ class Loader
                     setup_postdata($post);
 
                     $prev = get_previous_post();
+
+                    wp_reset_postdata();
+
+                    if (!$prev) {
+                        return null;
+                    }
+
+                    return $context
+                        ->get_loader('post')
+                        ->load_deferred($prev->ID);
+                },
+            ]);
+
+            // Next post in the same category field (new functionality)
+            register_graphql_field($typeName, 'nextInCategory', [
+                'type' => $typeName,
+                'description' => __('Next post in the same category'),
+                'args' => [
+                    'taxonomy' => [
+                        'type' => 'String',
+                        'description' => __('Taxonomy name. Default is "category".'),
+                        'defaultValue' => 'category'
+                    ]
+                ],
+                'resolve' => function (
+                    Post $post,
+                    array $args,
+                    AppContext $context
+                ) {
+                    global $post;
+
+                    // get post
+                    $post = get_post($post->ID, OBJECT);
+
+                    // setup global $post variable
+                    setup_postdata($post);
+
+                    // Get taxonomy from args or use default
+                    $taxonomy = isset($args['taxonomy']) ? $args['taxonomy'] : 'category';
+
+                    // Get next post in same category
+                    $next = get_next_post(true, '', $taxonomy);
+
+                    wp_reset_postdata();
+
+                    if (!$next) {
+                        return null;
+                    }
+
+                    return $context
+                        ->get_loader('post')
+                        ->load_deferred($next->ID);
+                },
+            ]);
+
+            // Previous post in the same category field (new functionality)
+            register_graphql_field($typeName, 'previousInCategory', [
+                'type' => $typeName,
+                'description' => __('Previous post in the same category'),
+                'args' => [
+                    'taxonomy' => [
+                        'type' => 'String',
+                        'description' => __('Taxonomy name. Default is "category".'),
+                        'defaultValue' => 'category'
+                    ]
+                ],
+                'resolve' => function (
+                    Post $post,
+                    array $args,
+                    AppContext $context
+                ) {
+                    global $post;
+
+                    // get post
+                    $post = get_post($post->ID, OBJECT);
+
+                    // setup global $post variable
+                    setup_postdata($post);
+
+                    // Get taxonomy from args or use default
+                    $taxonomy = isset($args['taxonomy']) ? $args['taxonomy'] : 'category';
+
+                    // Get previous post in same category
+                    $prev = get_previous_post(true, '', $taxonomy);
 
                     wp_reset_postdata();
 
